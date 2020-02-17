bbe0f3a3464b212030a00b7e6b4a025edbfd590a
Vaisakh Murali <vaisakhmurali@gmail.com>
2020-02-17 07:30:34 +0100

    sepolicy: Refactor for legacy inclusion
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- Android.mk
+++ Android.mk
@@ -2,73 +2,16 @@
 ifneq ($(TARGET_EXCLUDE_QCOM_SEPOLICY),true)
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 LOCAL_PATH:= $(call my-dir)
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
-    $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
-    $(LOCAL_PATH)/generic/public
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
-    $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
-    $(LOCAL_PATH)/generic/private
-
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
-    $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
-    $(LOCAL_PATH)/qva/public
-
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
-    $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
-    $(LOCAL_PATH)/qva/private
-
-# sepolicy rules for product images
-PRODUCT_PUBLIC_SEPOLICY_DIRS := \
-    $(PRODUCT_PUBLIC_SEPOLICY_DIRS) \
-     $(LOCAL_PATH)/product/public
-
-PRODUCT_PRIVATE_SEPOLICY_DIRS := \
-    $(PRODUCT_PRIVATE_SEPOLICY_DIRS) \
-     $(LOCAL_PATH)/product/private
-
-ifeq (,$(filter sdm845 sdm710 qcs605, $(TARGET_BOARD_PLATFORM)))
-    BOARD_SEPOLICY_DIRS := \
-       $(BOARD_SEPOLICY_DIRS) \
-       $(LOCAL_PATH) \
-       $(LOCAL_PATH)/generic/vendor/common \
-       $(LOCAL_PATH)/qva/vendor/common/sysmonapp \
-       $(LOCAL_PATH)/qva/vendor/ssg \
-       $(LOCAL_PATH)/timeservice \
-       $(LOCAL_PATH)/qva/vendor/qwesas \
-       $(LOCAL_PATH)/qva/vendor/common
-
-    ifeq ($(TARGET_SEPOLICY_DIR),)
-      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/generic/vendor/$(TARGET_BOARD_PLATFORM)
-      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/qva/vendor/$(TARGET_BOARD_PLATFORM)
-    else
-      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/generic/vendor/$(TARGET_SEPOLICY_DIR)
-      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/qva/vendor/$(TARGET_SEPOLICY_DIR)
-    endif
-
-    ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-    BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/generic/vendor/test
-    BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/qva/vendor/test
-    endif
+# New Gen boards
+ifeq (,$(filter sdm710 sdm845 msmnile trinket sm6150 kona lito atoll bengal, $(TARGET_BOARD_PLATFORM))
+include $(LOCAL_PATH)/sepolicy.mk
 endif
 
-ifneq (,$(filter sdm845 sdm710 qcs605, $(TARGET_BOARD_PLATFORM)))
-    BOARD_SEPOLICY_DIRS := \
-                 $(BOARD_SEPOLICY_DIRS) \
-                 $(LOCAL_PATH) \
-                 $(LOCAL_PATH)/legacy/vendor/common/sysmonapp \
-                 $(LOCAL_PATH)/legacy/vendor/ssg \
-                 $(LOCAL_PATH)/timeservice \
-                 $(LOCAL_PATH)/legacy/vendor/common
-
-    ifeq ($(TARGET_SEPOLICY_DIR),)
-      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/legacy/vendor/$(TARGET_BOARD_PLATFORM)
-    else
-      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/legacy/vendor/$(TARGET_SEPOLICY_DIR)
-    endif
-    ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-    BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/legacy/vendor/test
-    endif
+# Legacy boards
+ifeq (,($filter msm8974 msm8610 msm8226 apq8084 msm8916 msm8994 msm8909 msm8992 msm8996 msm8952 msm8937 msm8953 msm8998 sdm660, $(TARGET_BOARD_PLATFORM))
+include $(LOCAL_PATH)/legacy-um/sepolcy.mk
 endif
+
 endif
 endif
